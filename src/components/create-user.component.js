fad36c78e2ad9f87c4e46d819228bce8ce424000
Asfand <gulshierkhan@gmail.com>
2021-07-23 23:51:26 -0400

    master

---
+++ src/components/create-user.component.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class CreateUser extends Component {
+    render() {
+        return (
+            <div>
+                <p>Creat User Component</p>
+            </div>
+        )
+    }
+}
---
+++ src/components/edit-exercises.component.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class ExercisesList extends Component {
+    render() {
+        return (
+            <div>
+                <p>Edit Exercises List Component</p>
+            </div>
+        )
+    }
+}
